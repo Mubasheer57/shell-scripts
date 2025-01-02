d3e31ee3bf90e4e251e453d2189fb82305e89fb2
unknown <mubasheer57@gmail.com>
2025-01-02 11:05:41 +0530

    condition file

--- install.sh
+++ install.sh
@@ -10,20 +10,49 @@ fi
 
 dnf list installed mysql
 
- if [ $? -ne 0 ]
- then
-     dnf install mysql
-     echo "Installing MySQL ... FAILURE"
- else
-     echo "Installing MySQL ... SUCCESS"
- fi
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
+fi
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing MySQL ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing MySQL ... SUCCESS"
+# fi
 
 dnf list installed git
 
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Git ... FAILURE"
+        exit 1
+    else
+        echo "Installing Git ... SUCCESS"
+    fi
+else
+    echo "Git is already ... INSTALLED"
+fi
 
- if [ $? -ne 0 ]
- then
-     echo "Git already installed"
- else
-     echo "Installing Git ... SUCCESS"
- fi
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing Git ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing Git ... SUCCESS"
+# fi
