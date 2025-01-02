62664bda13abbc3a9a0fd9fbf74ff562eb00cdbc
unknown <mubasheer57@gmail.com>
2025-01-02 10:57:43 +0530

    condition file

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql
+
+ if [ $? -ne 0 ]
+ then
+     echo "Installing MySQL ... FAILURE"
+     exit 1
+ else
+     echo "Installing MySQL ... SUCCESS"
+ fi
+
+dnf list installed git
+
+
+ if [ $? -ne 0 ]
+ then
+     echo "Installing Git ... FAILURE"
+     exit 1
+ else
+     echo "Installing Git ... SUCCESS"
+ fi
